6ae740be89d117ac749b5d7a674d67f07698809e
ginkun <patrice.mauny@gmail.com>
2019-08-26 23:20:07 +0200

    Style configuration

--- osm2pgsql/import-osm.sh
+++ osm2pgsql/import-osm.sh
@@ -4,9 +4,10 @@ set -o pipefail
 set -o nounset
 
 readonly OSM_PBF_FILE="$IMPORT_DATA_DIR/import.pbf"
+readonly OSM_MAPZEN_STYLEFILE="$IMPORT_DATA_DIR/default.style"
 
 function exec_osm2pgsql() {
-    osm2pgsql -c -d "$POSTGRES_DB" -U "$POSTGRES_USER" -H "$POSTGRES_HOST" $OSM_PBF_FILE
+    osm2pgsql -c -d "$POSTGRES_DB" -U "$POSTGRES_USER" -H "$POSTGRES_HOST" -S "$OSM_MAPZEN_STYLEFILE"  $OSM_PBF_FILE
 }
 
 exec_osm2pgsql
