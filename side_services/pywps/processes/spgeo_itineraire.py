8cb9c0df041d025b292e3bf0dfa81495d43397d3
ginkun <patrice.mauny@gmail.com>
2019-11-04 21:42:33 +0000

    pyWOS process OK!

--- side_services/pywps/processes/spgeo_itineraire.py
+++ side_services/pywps/processes/spgeo_itineraire.py
@@ -1,13 +1,14 @@
-from pywps import Process, LiteralInput, ComplexOutput, Format
-import spgeo22
+import os, psycopg2
+from pywps import Process, LiteralInput, LiteralOutput, Format
 
 class AtoBRoute(Process):
     def __init__(self):
         inputs = [LiteralInput('x1', 'A point X', data_type='float'),
-                LiteralInput('y1', 'A point Y', data_type='float'),
-                LiteralInput('x2', 'B point X', data_type='float'),
-                LiteralInput('y2', 'B point Y', data_type='float'),]
-        outputs = [LiteralOutput('path', 'Route path from A to B', data_type='string')]
+                  LiteralInput('y1', 'A point Y', data_type='float'),
+                  LiteralInput('x2', 'B point X', data_type='float'),
+                  LiteralInput('y2', 'B point Y', data_type='float')]
+        outputs = [LiteralOutput(
+            'path', 'Route path from A to B', data_type='string')]
 
         super(AtoBRoute, self).__init__(
             self._handler,
@@ -19,13 +20,71 @@ class AtoBRoute(Process):
             store_supported=True,
             status_supported=True
         )
-    
-    def _handler(self, request, response):
-        route = spgeo22.proc_AtoB_Route.atob_route(\
-            request.inputs['x1'][0].data, \
-            request.inputs['y1'][0].data, \
-            request.inputs['x2'][0].data, \
-            request.inputs['y2'][0].data\
-                )
-        response.outputs['response'].data = route
+
+    @staticmethod
+    def _handler(request, response):
+        x1=request.inputs['x1'][0].data
+        y1=request.inputs['y1'][0].data
+        x2=request.inputs['x2'][0].data
+        y2=request.inputs['y2'][0].data
+
+        pg_link = None
+        cursor = None
+        # Get connection params from Shell
+        pg_user = os.environ.get('POSTGRES_USER')
+        pg_password = os.environ.get('POSTGRES_PASSWORD')
+        pg_host = os.environ.get('POSTGRES_HOST')
+        pg_port = os.environ.get('POSTGRES_PORT')
+        pg_database = os.environ.get('POSTGRES_DB')
+
+        # Create PostgreSQL connection
+        pg_link = psycopg2.connect(
+            user=pg_user, password=pg_password,     host=pg_host, port=pg_port,     database=pg_database)
+        cursor = pg_link.cursor()
+
+        ways_db = "ways"
+        cursor.execute(
+            "SELECT ST_MakeLine(geom) FROM pgr_atob_route(%s, %s, %s, %s, %s);", (ways_db, x1, y1, x2, y2))
+
+        # We get and store the resulting route
+        astarpath = cursor.fetchone()
+
+        # Close connection
+        if (pg_link):
+            cursor.close()
+            pg_link.close()
+
+        response.outputs['path'].data = astarpath
         return response
+
+def main():
+    x1=-1.6958886
+    y1=48.0582433
+    x2=-1.7628838
+    y2=48.1363986
+    # Get connection params from Shell
+    pg_user = os.environ.get('POSTGRES_USER')
+    pg_password = os.environ.get('POSTGRES_PASSWORD')
+    pg_host = os.environ.get('POSTGRES_HOST')
+    pg_port = os.environ.get('POSTGRES_PORT')
+    pg_database = os.environ.get('POSTGRES_DB')
+
+    # Create PostgreSQL connection
+    pg_link = psycopg2.connect(
+        user=pg_user, password=pg_password,     host=pg_host, port=pg_port,     database=pg_database)
+    cursor = pg_link.cursor()
+
+    ways_db = "ways"
+    cursor.execute(
+        "SELECT ST_MakeLine(geom) FROM pgr_atob_route(%s, %s, %s, %s, %s);", (ways_db, x1, y1, x2, y2))
+
+    # We get and store the resulting route
+    astarpath = cursor.fetchone()
+
+    cursor.close()
+    pg_link.close()
+    print("Connection to  PostgreSQL closed")
+    print(astarpath)
+
+if __name__ == "__main__":
+    main()
